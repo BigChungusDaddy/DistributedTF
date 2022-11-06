cc57ed76ff2a9c9bcfda3ba6bf3aa16050df312e
XIANGYAO TANG <u1311496@umail.utah.edu>
2022-11-06 14:40:55 -0700

    Added single node training code

--- main_dist.py
+++ main_dist.py
@@ -3,6 +3,7 @@ import os
 import sys
 import tensorflow as tf
 import mnist_setup
+import time
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 os.environ.pop('TF_CONFIG', None)
@@ -30,8 +31,10 @@ def main(worker):
     with strategy.scope():
     # Model building/compiling need to be within `strategy.scope()`.
         multi_worker_model = mnist_setup.build_and_compile_cnn_model()
-
-    multi_worker_model.fit(multi_worker_dataset, epochs=3, steps_per_epoch=70)
+    
+    start = time.time()
+    multi_worker_model.fit(multi_worker_dataset, epochs=100, steps_per_epoch=70)
+    print(time.time() - start)
 
 if __name__ == '__main__':
     main(int(sys.argv[1]))
