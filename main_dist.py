e7d2fd0435a86551452ff4f4752a2437c5359787
Axe Tang <u1311496@utah.edu>
2022-11-07 21:27:38 -0700

    Changed code so that user can input batchsize

--- main_dist.py
+++ main_dist.py
@@ -8,15 +8,15 @@ import time
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 os.environ.pop('TF_CONFIG', None)
 
-def main(worker):
+def main(worker, batch_size):
     tf_config = {
         'cluster': {
             'worker': ['192.168.1.1:12345', '192.168.1.2:23456']
             },
             'task': {'type': 'worker', 'index': 0}
         }
-    if worker == 1:
-        tf_config['task']['index'] = 1
+    if worker != 0:
+        tf_config['task']['index'] = worker
         
     os.environ['TF_CONFIG'] = json.dumps(tf_config)
     tf_config = json.loads(os.environ['TF_CONFIG'])
@@ -25,7 +25,7 @@ def main(worker):
     options = tf.data.Options()
     options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.AUTO
 
-    global_batch_size = 64
+    global_batch_size = batch_size
     multi_worker_dataset = mnist_setup.mnist_dataset(global_batch_size).with_options(options)
 
 
@@ -38,4 +38,4 @@ def main(worker):
     print(time.time() - start)
 
 if __name__ == '__main__':
-    main(int(sys.argv[1]))
+    main(int(sys.argv[1]), int(sys.argv[2]))
