b6780b3f17a0b2c61aec7a98eaf77c2ff7bf0eb7
Axe Tang <u1311496@utah.edu>
2022-11-22 10:33:39 -0700

    changed steps per epoch

--- main_nondist.py
+++ main_nondist.py
@@ -8,7 +8,7 @@ def main(batch_size):
     single_worker_dataset = mnist_setup.mnist_dataset(batch_size)
     single_worker_model = mnist_setup.build_and_compile_cnn_model()
     start = time.time()
-    single_worker_model.fit(single_worker_dataset, epochs=100, steps_per_epoch=75)
+    single_worker_model.fit(single_worker_dataset, epochs=100, steps_per_epoch=100)
     print(time.time() - start)
 
 if __name__ == '__main__':
